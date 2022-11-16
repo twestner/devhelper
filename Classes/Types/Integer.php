83543d44a062dc2eb59c3236bd895e78699dd4a6
Tina Westner <t.westner@taliteo.de>
2022-11-16 08:42:44 +0100

    [TASK] add type int

---
+++ Classes/Types/Integer.php
@@ -0,0 +1,49 @@
+<?php
+namespace Tw\Devhelper\Types;
+
+use Tw\Devhelper\Domain\Model\Configuration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class Integer extends AbstractType {
+    public function getSqlCode($configuration) {
+        return $configuration->getField() . ' int(11) DEFAULT 0 NOT NULL,';
+    }
+
+    public function getPropertyCode(Configuration $configuration)
+    {
+        return '/**
+     * @var integer
+     */
+    protected $' . GeneralUtility::underscoredToLowerCamelCase($configuration->getField()) . ' = \'\';';
+    }
+    public function getGetterAndSetterCode(Configuration $configuration)
+    {
+        $field = $configuration->getField();
+        $ucField = ucfirst($field);
+
+        return '    /**
+     * @return integer
+     */
+    public function get' . $ucField . '()
+    {
+        return $this->' . $field . ';
+    }
+
+    /**
+     * @param integer $' . $field . '
+     */
+    public function set' . $ucField . '($' . $field . ')
+    {
+        $this->' . $field . ' = $' . $field . ';
+    }';
+    }
+
+    public function getTcaFieldDefinition(Configuration $configuration)
+    {
+        $config = '\'type\' => \'input\',
+                \'size\' => 300,
+                \'eval\' => \'trim,int\'';
+
+        return $this->getTcaFieldDefinitionStructure($config, $configuration);
+    }
+}
